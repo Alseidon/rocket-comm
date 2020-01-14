63486795e1b63ad036dd31e631406a52d8d1c9d2
Alseidon <58395551+Alseidon@users.noreply.github.com>
2020-01-14 12:16:48 +0100

    file running the server

---
+++ test.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# version of : 25/12/2019
+# import socket
+#
+
+import rockcomm
+
+host = ''
+portD = 1111
+portC = 1112
+portU = 2111
+portI = 2112
+Serv = rockcomm.MainServer(host, portC, portD, portU, portI)
+Serv.start()
+
+
+# Serv.CThread.stopconnexion()
+# Serv.DThread.stopconnexion()
+# Serv.UThread.stopconnexion()
+# Serv.IThread.stopconnexion()
+# Serv.CThread.join()
+# Serv.CThread.join()
+# Serv.UThread.join()
+# Serv.IThread.join()
+
+# Serv.stop()
+    
+
+
+# class CMSCK():
+#     def __init__(self, sck):
+#         self.sck = sck
+    
+#     def close(self):
+#         self.sck += 2
+
+# class Thr():
+#     def __init__(self, sck):
+#         self.cmmsck = CMSCK(sck)
+    
+#     def close(self):
+#         self.cmmsck.close()
+#         return
